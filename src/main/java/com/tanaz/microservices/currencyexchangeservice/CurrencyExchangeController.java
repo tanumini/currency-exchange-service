2f446be5b0fccf6107ba62442ab931af5a508322
unknown <tanazshaik1709@gmail.com>
2024-03-08 18:04:20 +0530

    added load balancing and naming server and tracing

--- src/main/java/com/tanaz/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/tanaz/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.tanaz.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
   
+	private Logger logger =LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private Environment env;
 	
@@ -18,8 +22,13 @@ public class CurrencyExchangeController {
 	private ExchangeValueRepository repository;
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
-		
+		//642a564c9fd6f3184ee7dbaffd708fe4,a249be8d41eaa26d] [642a564c9fd6f3184ee7dbaffd708fe4-a249be8d41eaa26d]
+		logger.info("retriveExchangeValue call with {} to",from,to);
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+		if(exchangeValue==null) {
+			throw new RuntimeException("Unable to find data for "+from+"to"+to);
+		}
+		//exchangeValue.setPort(8000);
 		exchangeValue.setPort(
 				Integer.parseInt(env.getProperty("local.server.port")));
 		return exchangeValue;
